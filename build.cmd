3b084dc80673d5148bd1e567adec9899f23033a1
ope-nz <github@ope.nz>
2026-05-04 18:19:51 +1200

    PushPull update

---
+++ build.cmd
@@ -0,0 +1,15 @@
+@echo off
+setlocal
+
+set CSC=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe
+set OUT=B4JScanner.exe
+set REFS=/r:System.IO.Compression.dll /r:System.IO.Compression.FileSystem.dll /r:System.Xml.dll /r:System.Windows.Forms.dll /r:System.Drawing.dll
+
+%CSC% /target:winexe /out:%OUT% /win32icon:icon.ico %REFS% Program.cs Models.cs B4JProjectParser.cs LibraryResolver.cs JarAnalyzer.cs JavaSourceScanner.cs SbomWriter.cs MdWriter.cs MainForm.cs Config.cs
+
+if errorlevel 1 (
+    echo Build FAILED
+    exit /b 1
+)
+
+echo Build OK: %OUT%
