88b8e01b6042026be5cb25452457a9664ba7ff05
ope-nz <github@ope.nz>
2026-05-04 18:20:20 +1200

    PushPull update

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using System.Windows.Forms;
+
+namespace B4JScanner
+{
+    class Program
+    {
+        [System.STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
