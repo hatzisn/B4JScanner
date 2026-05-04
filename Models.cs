4f599a0f3f54bf88ae9b2ff748f46004a548f240
ope-nz <github@ope.nz>
2026-05-04 18:20:01 +1200

    PushPull update

---
+++ Models.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+
+namespace B4JScanner
+{
+    class B4JProject
+    {
+        public string ProjectFile { get; set; }
+        public string ProjectFolder { get; set; }
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public string JavaPackage { get; set; }
+        public List<string> Libraries { get; set; }
+        public List<string> Modules { get; set; }
+
+        public List<string> AdditionalJars { get; set; }
+
+        public B4JProject()
+        {
+            Libraries      = new List<string>();
+            Modules        = new List<string>();
+            AdditionalJars = new List<string>();
+        }
+    }
+
+    class ResolvedLibrary
+    {
+        public string LibraryName { get; set; }
+        public string JarPath { get; set; }
+        public string XmlPath { get; set; }
+        public string B4xlibPath { get; set; }
+        public bool IsAdditionalJar { get; set; }
+        public bool Found { get { return JarPath != null || B4xlibPath != null; } }
+        public LibraryInfo Info { get; set; }
+    }
+
+    class LibraryInfo
+    {
+        public string DisplayName { get; set; }
+        public string Version { get; set; }
+        public string Vendor { get; set; }
+        public string Description { get; set; }
+        public string VersionSource { get; set; }
+        public List<string> Dependencies { get; set; }
+        public MavenCoords Maven { get; set; }
+        public List<ResolvedDependency> ResolvedDeps { get; set; }
+
+        public LibraryInfo()
+        {
+            Dependencies = new List<string>();
+            ResolvedDeps = new List<ResolvedDependency>();
+        }
+    }
+
+    class MavenCoords
+    {
+        public string GroupId { get; set; }
+        public string ArtifactId { get; set; }
+        public string Version { get; set; }
+
+        public string ToPurl()
+        {
+            return "pkg:maven/" + Uri.EscapeDataString(GroupId ?? "unknown")
+                 + "/" + Uri.EscapeDataString(ArtifactId ?? "unknown")
+                 + "@" + Uri.EscapeDataString(Version ?? "unknown");
+        }
+    }
+
+    class ResolvedDependency
+    {
+        public string Name { get; set; }
+        public string JarPath { get; set; }
+        public MavenCoords Maven { get; set; }
+    }
+
+    class JavaSourceFile
+    {
+        public string FilePath { get; set; }
+        public string ClassName { get; set; }
+        public string Package { get; set; }
+        public List<string> Imports { get; set; }
+
+        public JavaSourceFile()
+        {
+            Imports = new List<string>();
+        }
+    }
+}
